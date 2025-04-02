1fee29555b346d1915e6e599d53b41b07687825d
Wilko Quak <w.quak@geonovum.nl>
2025-04-02 13:54:43 +0200

    kolom herkomst toegevoegd.

--- Validatiematrix2markdown.py
+++ Validatiematrix2markdown.py
@@ -17,6 +17,11 @@ outfile = open('04-Validatiematrix.md','w')
 print("""
 # De validatiematrix
 
+De validatiematrix hoort niet bij een specifieke versie van de STOP of IMOW
+standaard. Om te weten of een validatie geldig is of niet kun je naar de kolom
+herkomst kijken. Deze geeft aan vanaf welke versie de validatie in de standaard
+zit.
+
 De volgende versie van de standaarden zijn gebruikt voor het samenstellen van de validatiematrix:
  - Informatiemodel Omgevingswet (IMOW) Versie 2.0.2.
  - STOP standaard Versie 1.3.0.
@@ -28,13 +33,14 @@ Betekenis van de kolommen:
 |---------------|--------------|
 | identificatie | Unieke identificatie van de validatieregel die gebruikt kan worden in communicatie over de regel. |
 | ernst         | 'Blokkerend' of 'Waarschuwing'. Blokkerende regels leiden tot afkeuring van het document in de keten. Een waarschuwing resulteert niet tot afkeuring van het document maar levert een melding bij de indiener van het document. |
+| herkomst      | Verwijzing naar standaard(versie) waar deze validatie zijn oorsprong vindt. |
 | omschrijving  | Eenduidige omschrijving van de validatieregel. |
 
 
 De validatiematrix bevat de volgende validatieregels:
 
-| Identificatie | Ernst | Omschrijving|
-|:--------------|:------|:------------|""",file=outfile)
+| Identificatie | Ernst | Herkomst | Omschrijving|
+|:--------------|:------|:---------|:------------|""",file=outfile)
 
 wb = openpyxl.load_workbook('Validatiematrix.xlsx')
 sheet = wb['Validatieregels']
@@ -43,9 +49,10 @@ for row in sheet.iter_rows():
         continue
 
     id = str(row[0].value) 
-    ernst = str(row[3].value)
+    ernst = str(row[2].value)
+    herkomst = str(row[3].value)
     if ernst != 'Blokkerend' and ernst != 'Waarschuwing':
         print("ERROR: ernst moet 'Blokkerend' of 'Waarschuwing' zijn voor: " + id, file=sys.stderr)
     regel = row[1].value.replace('\n',' ').replace('<','&lt;').replace('>','&gt;')
-    print('|' + id + '|' + ernst + '|' + regel + '|',file=outfile)
+    print('|' + id + '|' + ernst + '|' + herkomst + '|' + regel + '|',file=outfile)
 outfile.close()
