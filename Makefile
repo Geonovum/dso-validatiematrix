9d54dc84ff5554995d150655a69e0db8e2081253
Wilko Quak <w.quak@geonovum.nl>
2025-06-05 14:37:57 +0200

    TPOD validatie uit spreadsheet naar ValidatiesTPOD*.md

--- Makefile
+++ Makefile
@@ -2,5 +2,19 @@
 # Dit programma maakt gebruik van openpyxl
 # probeer: sudo apt install python3-openpyxl
 #
+all: 04-Validatiematrix.md ValidatiesLVBB.md ValidatiesTPOD.md
 04-Validatiematrix.md: Validatiematrix.xlsx Validatiematrix2markdown.py
 	./Validatiematrix2markdown.py
+
+ValidatiesLVBB.md: ValidatiesLVBB.xlsx ValidatiesLVBB2markdown.py
+	./ValidatiesLVBB2markdown.py
+
+ValidatiesTPOD.md: ../ow-IMOW/constraints.md ValidatiesTPOD-catalogus.md ValidatieTPOD-tpods.md
+	echo '# Validaties TPOD' > ValidatiesTPOD.md
+	echo '' >> ValidatiesTPOD.md
+	echo '| id  | validatie |' >> ValidatiesTPOD.md
+	echo '|-----|---------- |' >> ValidatiesTPOD.md
+	cat ../ow-IMOW/constraints.md ValidatiesTPOD-catalogus.md ValidatieTPOD-tpods.md | sort  >> ValidatiesTPOD.md
+
+clean:
+	rm -f ValidatiesTPOD.md 04-Validatiematrix.md ValidatiesLVBB.md
---
+++ ValidatiesLVBB2markdown.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+#
+# File: ValidatiesLVBB2markdown.py
+#
+# Omschrijving: De validaties van de LVBB worden beheerd in een excel document en gepubliceerd als markdown.
+# Dit script doet de conversie.
+#
+# Auteur: w.quak@geonovum.nl
+
+
+from datetime import datetime
+import openpyxl
+import sys
+
+outfile = open('ValidatiesLVBB.md','w')
+
+print("""
+# Validaties geïmplementeerd door de de LVBB
+
+De LVBB heeft de volgende validaties geïmplementeerd:
+
+| Identificatie | Ernst | Omschrijving|
+|:--------------|:------|:------------|""",file=outfile)
+
+wb = openpyxl.load_workbook('ValidatiesLVBB.xlsx')
+sheet = wb['1.5.1 validaties']
+for row in sheet.iter_rows():
+    if not (row[2].value):
+        print("LOG: skipping empty row: ", file=sys.stderr)
+        continue
+
+    id = str(row[2].value) 
+    
+    if not (id.startswith('LVBB') or id.startswith('BHKV') or id.startswith('STOP') or id.startswith('TPOD')):
+        print("LOG: skipping row: " + str(row[2].row) + " with id = " + row[2].value, file=sys.stderr)
+        continue
+    if not (row[3].value):
+        print("LOG: skipping rule with empty value : " + id , file=sys.stderr)
+        continue
+
+    ernst = str(row[7].value)
+    herkomst = str(row[5].value)
+    if ernst != 'Blokkerend' and ernst != 'Waarschuwing':
+        print("ERROR: ernst moet 'Blokkerend' of 'Waarschuwing' zijn voor: " + id, file=sys.stderr)
+    regel = row[3].value.replace('\n',' ').replace('<','&lt;').replace('>','&gt;')
+    print('|' + id + '|' + ernst + '|' + regel + '|',file=outfile)
+outfile.close()
